e1121285a9a789a74e8e1d10137f82d679e24015
darsen <dlya_web_konkursa@mail.ru>
2022-02-09 20:12:42 +0300

    v 1.0.1

--- src/components/router/MainRouter.jsx
+++ src/components/router/MainRouter.jsx
@@ -1,18 +1,14 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Loader from '../UI/Loader/Loader'
+import MainPage from '../pages/MainPage/MainPage'
+import NotFound from '../pages/NotFound/NotFound'
 
-const MainPage = React.lazy(() => import('../pages/MainPage/MainPage'))
-const NotFound = React.lazy(() => import('../pages/NotFound/NotFound'))
-
-const MainRouter = ({setIsLoading}) => {
+const MainRouter = () => {
     return (
-        <Suspense fallback={<Loader setIsLoading={setIsLoading}/>}>
-            <Routes>
-                <Route path={'/'} element={<MainPage/>}/>
-                <Route path={'*'} element={<NotFound/>}/>
-            </Routes>
-        </Suspense>
+        <Routes>
+            <Route path={'/'} element={<MainPage/>}/>
+            <Route path={'*'} element={<NotFound/>}/>
+        </Routes>
     )
 }
 
