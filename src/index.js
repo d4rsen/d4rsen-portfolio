44f166957c40165659ed0b67b2d9c862d5259270
darsen <dlya_web_konkursa@mail.ru>
2022-02-10 10:34:08 +0300

    v 1.0.4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App/App'
 import './normalize.css'
+
+ReactGA.initialize('G-303075485', {debug: true})
 
 ReactDOM.render(
     <React.StrictMode>
