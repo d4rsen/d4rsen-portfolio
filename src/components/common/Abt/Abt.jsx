c29a1451aba24c52c4750a88f410bb014957348a
d4rsen <darsenungarlinov00@gmail.com>
2022-06-27 12:09:45 +0300

    svg background image sizes increased

--- src/components/common/Abt/Abt.jsx
+++ src/components/common/Abt/Abt.jsx
@@ -1,22 +1,22 @@
-import React from "react"
-import SkillsItem from "../SkillsItem/SkillsItem"
+import React from 'react'
+import SkillsItem from '../SkillsItem/SkillsItem'
 
 function Abt() {
     return (
         <>
             <div className="jobs__title title">
-                <span>Middle Frontend developer</span>
+                <span>Frontend developer</span>
                 <span className="title__company">
-                    {" Alpha Business Technologies"}
+                    {' Alpha Business Technologies'}
                 </span>
             </div>
             <div className="jobs__subtitle">July 2021 - Now</div>
             <SkillsItem
-                text="Write modern, performant, maintainable code for a diverse array of client and internal projects" />
+                text="Write modern, performant, maintainable code for a diverse array of client and internal projects"/>
             <SkillsItem
-                text="Work with a variety of platforms, frameworks, and content management systems such as JavaScript, TypeScript, React" />
+                text="Work with a variety of platforms, frameworks, and content management systems such as JavaScript, TypeScript, React"/>
             <SkillsItem
-                text="Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis" />
+                text="Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis"/>
         </>
     )
 }
