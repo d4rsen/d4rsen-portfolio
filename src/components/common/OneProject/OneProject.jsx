66f1d67e20f98f5ec141b0920b3fe2344707d54b
d4rsen <darsenungarlinov00@gmail.com>
2022-06-27 11:33:25 +0300

    v1.1.4

--- src/components/common/OneProject/OneProject.jsx
+++ src/components/common/OneProject/OneProject.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import "./OneProject.scss"
+import React from 'react'
+import './OneProject.scss'
 
 function OneProject({ title, text, stackItems, imgSrc, gitLink, projectLink }) {
     return (
@@ -36,7 +36,7 @@ function OneProject({ title, text, stackItems, imgSrc, gitLink, projectLink }) {
                         >
                             <title>GitHub</title>
                             <path
-                                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+                                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
                         </svg>
                     </a>
                     <a
@@ -57,15 +57,15 @@ function OneProject({ title, text, stackItems, imgSrc, gitLink, projectLink }) {
                             className="feather feather-external-link"
                         >
                             <title>External Link</title>
-                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-                            <polyline points="15 3 21 3 21 9" />
-                            <line x1="10" y1="14" x2="21" y2="3" />
+                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
+                            <polyline points="15 3 21 3 21 9"/>
+                            <line x1="10" y1="14" x2="21" y2="3"/>
                         </svg>
                     </a>
                 </div>
             </div>
             <div className="project__right right">
-                <a href={projectLink} target="_blank" rel="noreferrer">
+                <div>
                     <div className="right__wrapper">
                         <div className="right__imageWrapper">
                             <a
@@ -79,11 +79,11 @@ function OneProject({ title, text, stackItems, imgSrc, gitLink, projectLink }) {
                                     src={imgSrc && imgSrc}
                                     alt="project"
                                 />
-                                <div className="right__hidden" />
+                                <div className="right__hidden"/>
                             </a>
                         </div>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
     )
