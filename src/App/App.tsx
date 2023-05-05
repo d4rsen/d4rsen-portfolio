26c3726dde03e7a18d4a5ec3b2febe7235de4b50
Ungarlinov Darsen S <dungarli@megafon.ru>
2023-05-05 15:23:35 +0300

    package scroll-percentage deleted

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,30 +8,25 @@ import Header from '../components/organisms/Header/Header';
 import Router from '../router/Router';
 import { menu } from '../store/menu.store';
 import './App.scss';
-import LoadingBar from 'react-top-loading-bar'; // @ts-ignore
+// @ts-ignore
 import { init } from './canvas';
 import { Cursor } from '../components/atoms/Cursor/Cursor';
-import { useScrollPercentage } from 'react-scroll-percentage';
 
 const App = observer(() => {
     const [isLoading, setIsLoading] = useState(true);
-    const [scrollRef, percentage] = useScrollPercentage();
 
     useEffect(() => {
         setTimeout(() => setIsLoading(false), 4000);
         init();
     }, []);
 
-    const calculatedPercentage = Number((percentage * 100).toFixed()) + 14;
-
     if (isLoading) {
         return <Loader />;
     }
 
     return (
-        <div className="wrapper" ref={scrollRef}>
+        <div className="wrapper">
             <Background />
-            <LoadingBar color={'#64ffda'} height={3} progress={calculatedPercentage} />
             <Cursor />
             <div className="_main-container">
                 <Header />
