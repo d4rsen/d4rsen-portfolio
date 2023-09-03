7ac718113769b65a74dedb14a9b5effdadadf46b
Darsen <darsenungarlinov00@gmail.com>
2023-09-03 21:00:59 +0300

    Update MainPage.tsx

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -13,7 +13,7 @@ function MainPage() {
                 <About />
                 <Jobs />
                 <Projects />
-                <Contact />
+                {/* <Contact /> */}
             </div>
         </div>
     );
