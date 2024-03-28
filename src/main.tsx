625d91382ae85c0894f14e64d112ab57777567ef
d4rsen <darsenungarlinov@MacBook-Pro-Darsen.local>
2024-03-28 10:51:42 +0300

    update packages and delete main black hole

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App/App';
 import ErrorBoundary from './components/atoms/ErrorBoundary';
-import { isMobile } from 'react-device-detect';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <BrowserRouter>
         <ErrorBoundary>
-            <App />
+            <App/>
             <div id="c"></div>
             <div id="stars"></div>
             <div id="stars2"></div>
             <div id="stars3"></div>
-            {!isMobile && (
-                <div className="blackhole">
-                    <div className="megna">
-                        <div className="black"></div>
-                    </div>
-                </div>
-            )}
         </ErrorBoundary>
     </BrowserRouter>
 );
